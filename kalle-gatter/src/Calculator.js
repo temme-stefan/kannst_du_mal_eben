8dca894486947dbb829b6aa179b3f84c57c13cb2
Stefan Temme <temme@objectcode.de>
2022-09-29 21:07:37 +0200

    Up and running

--- kalle-gatter/src/Calculator.js
+++ kalle-gatter/src/Calculator.js
@@ -1,15 +1,60 @@
 import {useState} from "react";
+import {generateMatrizes} from "./lib/MatrixGenerator.mjs";
+import './Calculator.css'
 
+function Matrix({matrix}) {
+    // console.log(props);
+    return (<table>
+            <tbody>
+            <tr>
+                <td>
+                    {matrix[0][0]}
+                </td>
+                <td>
+                    {matrix[1][0]}
+                </td>
+                <td>
+                    {matrix[2][0]}
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    {matrix[0][1]}
+                </td>
+                <td>
+                    {matrix[1][1]}
+                </td>
+                <td>
+                    {matrix[2][1]}
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    {matrix[0][2]}
+                </td>
+                <td>
+                    {matrix[1][2]}
+                </td>
+                <td>
+                    {matrix[2][2]}
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    );
+}
 
 
 function Calculator() {
-    const [abc,setAbc] = useState({a:"a",b:"b",c:"c"});
-    const handleSubmit= (e)=>{
+    const [abc, setAbc] = useState({a: "a", b: "b", c: "c"});
+    const handleSubmit = (e) => {
         e.preventDefault();
         const formData = new FormData(e.target);
         const formProps = Object.fromEntries(formData);
         setAbc(formProps);
     };
+    const matrizen = generateMatrizes(abc.a, abc.b, abc.c);
+
     return (
         <>
             <form id={"inputs"} onSubmit={handleSubmit}>
@@ -19,7 +64,7 @@ function Calculator() {
                 <button type="submit">Aktualisieren</button>
             </form>
             <output>
-
+                {matrizen.map((mat,i) => (<Matrix key={"matrix_"+i} matrix={mat}/>))}
             </output>
         </>
     );
